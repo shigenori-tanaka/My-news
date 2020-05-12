183eb2e2499159751c259fb4d80da63aa8366e92
tanaka <s.tanaka0917@gmail.com>
2020-05-12 19:02:01 +0000

    モデルHistoryを関連付け

--- app/News.php
+++ app/News.php
@@ -7,10 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     protected $guarded = array('id');
-    
-    // データ入力が異常な時に、保存しないで戻す
+
+    // 以下を追記
     public static $rules = array(
         'title' => 'required',
         'body' => 'required',
     );
+
+    // 以下を追記
+    // Newsモデルに関連付けを行う
+    public function histories()
+    {
+      return $this->hasMany('App\History');
+
+    }
 }
